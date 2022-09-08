a4f296cae536b0fbc6f8b5b7b78304a85735cb0a
Shreeya Sethi <shreeyas@ad.unc.edu>
2022-09-08 11:47:56 -0400

    changes

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -4,32 +4,32 @@ __author__ = "730553668"
 
 secret_word: str = "python" 
 guess: str = input(f"What is your {len(secret_word)} letter guess?")
-#is the guessed word the correct number of characters
+# is the guessed word the correct number of characters
 while len(guess) != len(secret_word): 
     guess = input(f"That is not {len(secret_word)} letters! Try again: ")
 # variables definition used in the while loop
-index = 0 
-emoji = ""
-green = "\U0001F7E9"
-yellow = "\U0001F7E8"
-white = "\U00002B1C" 
+index: int = 0 
+emoji: str = ""
+green: str = "\U0001F7E9"
+yellow: str = "\U0001F7E8"
+white: str = "\U00002B1C" 
 # green, yellow, white emoji 
 while index < len(secret_word):
-    match_character = False
-    secret_index = 0 
+    match_character: bool = False
+    secret_index: int = 0 
 # if the character in guess and secret_word in the same index is the same, printing green block 
 # else going into a while loop to find out if the guess character matches the character of the secret_word on a different index
 # if neither, printing the white block 
     if guess[index] == secret_word[index]:
         emoji += green
     else:
-        while match_character is False and secret_index < len(secret_word):
+        while match_character and secret_index < len(secret_word):
             if secret_word[secret_index] == guess[index]:
-                match_character is True
+                match_character = True
             else:
                 secret_index += 1
 
-        if match_character is True:
+        if match_character:
             emoji += yellow
         else:
             emoji += white
