9020e2574ccb7ecbf753675eef064f876b96a95b
Shreeya Sethi <shreeyas@ad.unc.edu>
2022-09-13 10:43:15 -0400

    Ex03

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -4,12 +4,10 @@ __author__ = "730553668"
 
 
 def contains_char(main_string: str, one_char: str) -> bool: 
-    # main_string is the string being searched through while one_char is the string being searched for. 
     """Looking for the presence of a charcater in a longer string."""
     assert len(one_char) == 1 
     index: int = 0
     while index < len(main_string):
-        # checking for the one character and exiting the while loop when the main_string ends
         if main_string[index] == one_char:
             return True 
         else: 
@@ -17,7 +15,6 @@ def contains_char(main_string: str, one_char: str) -> bool:
     return False 
 
 
-# string code for the three color emojis
 green: str = "\U0001F7E9"
 yellow: str = "\U0001F7E8"
 white: str = "\U00002B1C" 
@@ -29,13 +26,13 @@ def emojified(guess: str, secret: str) -> str:
     i: int = 0 
     emoji: str = ""
     while i < len(secret): 
-        if guess[i] == secret[i]: 
-            emoji += green 
-        elif contains_char(secret, guess[i]): 
-        # if the character is found in the word but the index isn't the same yellow box is printed
-            emoji += yellow 
+        if contains_char(secret, guess[i]): 
+            if secret[i] == guess[i]:
+                emoji += green 
+            else: 
+                emoji += yellow
         else: 
-            emoji += white
+            emoji += white 
         i += 1
     return emoji
 
@@ -50,16 +47,14 @@ def input_guess(guess_expected_len: int) -> str:
 
 def main() -> None:
     """The entrypoint of the program and main game loop."""
-    # defining required variables 
     secret: str = "codes"
     turn: int = 0
     win: bool = False
     while turn < 6 and not win:
         turn += 1 
         print("=== Turn " + str(turn) + "/6 " + "===")
-        entered_word: str = input_guess(5)
+        entered_word: str = input_guess(len(secret))
         emojis: str = emojified(entered_word, secret)
-        # when the guessed word is equal to the secret word
         if entered_word == secret:
             win = True
         print(emojis)         
