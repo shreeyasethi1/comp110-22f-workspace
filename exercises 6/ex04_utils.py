2100bc2ab1e1b42e48399fae3efcc267c80597fb
Shreeya Sethi <shreeyas@ad.unc.edu>
2022-09-18 20:52:09 -0400

    04

--- exercises 6/ex04_utils.py
+++ exercises 6/ex04_utils.py
@@ -4,20 +4,20 @@ __author__ = "730553668"
 
 
 def all(input:list[int] , one:int) -> bool: 
-    """All numbers in the list should be the same as the inputed number.""" 
+    """all numbers in the list should be the same as the inputed number.""" 
     i: int = 0
-    if len(input) == 0:  
+    if len(input) == 0: 
         return False
     while i < len(input): 
         if input[i] == one: 
             i += 1
-        else:   
+        else:  
             return False
     return True
 
 
 def max(input: list[int]) -> int: 
-    """Selecting the greatest number in the list."""
+    """selecting the greatest number in the list."""
     if len(input) == 0: 
         raise ValueError("max() arg is an empty List")
     i: int = 0 
@@ -30,14 +30,17 @@ def max(input: list[int]) -> int:
     return largest 
 
 
-def is_equal(l1: list[int] , l2: list[int]) -> bool:  
-    """Checking that the two lists inputed are the exact same."""
+def is_equal(l1: list[int], l2: list[int]) -> bool:  
+    """checking that the two lists inputed are the exact same."""
     i: int = 0
-    if len(l1) == 0 or len(l2) == 0:  
-        return False 
+    if len(l1) == 0 or len(l2) == 0: 
+        return False
     while i < (len(l1) - 1):  
         if l1[i] == l2[i]: 
             i += 1
         else:  
             return False 
     return True
+
+
+print(is_equal([1,0,2],[]))
